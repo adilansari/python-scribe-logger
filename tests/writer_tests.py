1514c22879c2d1f44b19cc65b358df3473b993c5
Adil Ansari <ansari.adil20@gmail.com>
2015-04-19 15:58:06 -0700

    writer and logger tests

---
+++ tests/writer_tests.py
@@ -0,0 +1,38 @@
+from scribe_logger.writer import ScribeWriter
+from scribe_logger.connection import Connection
+from collections import defaultdict
+from unittest import TestCase
+from mock import Mock
+from json import dumps
+from thrift.Thrift import TException
+
+
+class WriterTestCase(TestCase):
+
+    HOST = 'localhost'
+    PORT = 1464
+
+    def setUp(self):
+        self.mock_connection = Mock(spec=Connection)
+        self.mock_connection.send = lambda self: True
+        self.writer = ScribeWriter(self.HOST, self.PORT)
+        self.writer.client = self.mock_connection
+
+    def test_invalid_writes(self):
+        self.assertRaises(TypeError, self.writer.write)
+        self.assertRaises(ValueError, self.writer.write, ['message', defaultdict()])
+        self.assertRaises(ValueError, self.writer.write, defaultdict())
+        self.assertRaises(ValueError, self.writer.write, {'k': 'v'})
+        self.assertRaises(ValueError, self.writer.write, 12345)
+
+    def test_valid_writes(self):
+        self.assertTrue(self.writer.write('message'))
+        self.assertTrue(self.writer.write(['m1', 'm2']))
+        self.assertTrue(self.writer.write([]))
+        self.assertTrue(self.writer.write([dumps(defaultdict()), 'message']))
+        self.assertTrue(self.writer.write(dumps(defaultdict())))
+
+    def test_connection_error(self):
+        self.writer = ScribeWriter(self.HOST, self.PORT)
+        with self.assertRaises(TException):
+            self.writer.write('message')
