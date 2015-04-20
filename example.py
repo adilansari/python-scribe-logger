c518305610c3b3682f417ba68a3af154ab90f1ba
Adil Ansari <ansari.adil20@gmail.com>
2015-04-19 21:43:46 -0700

    license and example

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ import logging
 my_logger = logging.getLogger('MyLogger')
 my_logger.setLevel(logging.DEBUG)
 
-scribe = ScribeLogHandler(category="test_category")
+scribe = ScribeLogHandler('localhost', 1464, category='test_category')
 scribe.setLevel(logging.DEBUG)
 my_logger.addHandler(scribe)
 
-my_logger.info("This is a test message")
+my_logger.info('This is a test message')
